e2e82428848570a431d2c5a977412821ec072be0
Tim Pesockiy <pesockiy.timur@gmail.com>
2021-05-03 23:09:12 +0300

    creating sections

--- js/index.js
+++ js/index.js
@@ -1,21 +1,19 @@
 const swiper = new Swiper('.swiper-container', {
-    // Optional parameters
-    // direction: 'vertical',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
+  speed: 400,
+  navigation: {
+    disabledClass: 'disabled',
+    nextEl: '.arrow-next',
+    prevEl: '.arrow-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'fraction',
+    renderFraction: function (currentClass, totalClass) {
+      return '<span class="' + currentClass + '"></span>' +
+      '<span class="separator"></span>' +
+        '<span class="' + totalClass + '"></span>';
     },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
+  },
+
+
+});
